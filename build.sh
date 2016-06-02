474ccd969155b82232d7947d63ecca4edd083b0a
Tony Pujals <tony@subfuzion.com>
2016-06-01 17:48:42 -0700

    simplified building thanks to vendor support

--- build.sh
+++ build.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 #
-# This script is used to build an image (build-envtpl) which is then used
-# to build the envtpl binary for Linux. The binary can then be copied
-# into other images that need to use it.
+# This script will build the executable and leave it in this directory.
 #
 # If the first argument to the script is set to "alpine", then it will
 # build a binary for Alpine Linux.
 #
-set -e
 
-# build the "builder" image
-if [ $1 = "alpine" ]; then
-  docker build -t build-envtpl -f build/alpine/Dockerfile .
-else
-  docker build -t build-envtpl -f build/Dockerfile .
-fi
+IMAGE=$([ "$1" == "alpine" ] && echo "golang:alpine" || echo "golang")
+SRCDIR=/usr/local/go/src/github.com/subfuzion/envtpl
 
-# run a builder container to create the envtpl binary and save to the
-# current directory
-docker run --rm -v "$PWD":/usr/local/go/src/github.com/subfuzion/envtpl -w /usr/local/go/src/github.com/subfuzion/envtpl build-envtpl
+docker run --rm -v "$PWD":$SRCDIR -w $SRCDIR $IMAGE go build -v .
